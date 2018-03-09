551f6ea1a3f03da87579558ffe91e4b5c6105eed
Stephen <stephen@mapsmith.net>
2018-03-09 15:27:36 -0500

    url parser
    still buggy!

--- app.js
+++ app.js
@@ -10,9 +10,30 @@ require([
     "esri/Map",
     "esri/views/MapView",
     "esri/layers/VectorTileLayer",
-	"esri/layers/FeatureLayer",
+	  "esri/layers/FeatureLayer",
+    "esri/geometry",
+    "esri/core/urlUtils",
     "dojo/domReady!"
-  ], function(Map, MapView, FeatureLayer, VectorTileLayer) {
+  ], function(Map, MapView, VectorTileLayer, FeatureLayer,
+  geometry, urlUtils) {
+
+    //if there are url params zoom to location
+         var coords, zoomLevel;
+         var urlObject = esri.urlToObject(document.location.href);
+
+
+        if(urlObject.query && urlObject.query.coords && urlObject.query.zoomLevel){
+          var coords = urlObject.query.coords.split(',');
+
+          var lon = parseFloat(coords[0]);
+          var lat = parseFloat(coords[1]);
+
+
+          var zoomLevel = parseInt(urlObject.query.zoomLevel);
+          var point = esri.geometry.geographicToWebMercator(new esri.geometry.Point(lon,lat));
+
+          map.centerAndZoom(point,zoomLevel);
+        }
 
     var map = new Map({
       basemap: "osm"
