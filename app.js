b75ac22aa12f2001302477224a7e0f43db8270d5
josiah <josiah.raiche@vermont.gov>
2018-04-03 08:06:43 -0400

    new way of doing zoom levels

--- app.js
+++ app.js
@@ -76,7 +76,67 @@ require([
       map: map,
       center: [lon,lat],
       zoom: zoom,
-	  maxZoom:0
+	  lods:[ {
+          level: 10,
+          resolution: 152.874056570411,
+          scale: 577790.554289
+        },
+        {
+          level: 11,
+          resolution: 76.4370282850732,
+          scale: 288895.277144
+        },
+        {
+          level: 12,
+          resolution: 38.2185141425366,
+          scale: 144447.638572
+        },
+        {
+          level: 13,
+          resolution: 19.1092570712683,
+          scale: 72223.819286
+        },
+        {
+          level: 14,
+          resolution: 9.55462853563415,
+          scale: 36111.909643
+        },
+        {
+          level: 15,
+          resolution: 4.77731426794937,
+          scale: 18055.954822
+        },
+        {
+          level: 16,
+          resolution: 2.38865713397468,
+          scale: 9027.977411
+        },
+        {
+          level: 17,
+          resolution: 1.19432856685505,
+          scale: 4513.988705
+        },
+        {
+          level: 18,
+          resolution: 0.597164283559817,
+          scale: 2256.994353
+        },
+        {
+          level: 19,
+          resolution: 0.298582141647617,
+          scale: 1128.497176
+        },
+        {
+          level: 20,
+          resolution: 0.1492910708238085,
+          scale: 564.248587
+        },
+        {
+          level: 21,
+          resolution: 0.0746455354119043,
+          scale: 282.1242935
+        }
+      ]
     });
 
     // var mapillary = new VectorTileLayer({
