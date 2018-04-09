dce67fedd06e51c79f04e018ed7a9cdd1e53a7ff
josiah <josiah.raiche@vermont.gov>
2018-04-09 08:19:48 -0400

    Basemap Toggle, tabs to spaces

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ function getParameterByName(name) {
 }
 
 function mapillaryLink(){
-	var data = document.getElementsByClassName('mapillary')[0].data || view.center,
-		lat = data.latitude,
-		lon = data.longitude;
+    var data = document.getElementsByClassName('mapillary')[0].data || view.center,
+        lat = data.latitude,
+        lon = data.longitude;
 
-	// url https://www.mapillary.com/app/?username=vtrans_row&lat=44&lng=-72&z=7.5
-	var mapillarypre = 'https://www.mapillary.com/app/?username=vtrans_row&lat='
-	var mapillary2nd = '&lng='
-	var mapillary3rd = '&z='
-	var mapillarySigns = '&signs=true'
+    // url https://www.mapillary.com/app/?username=vtrans_row&lat=44&lng=-72&z=7.5
+    var mapillarypre = 'https://www.mapillary.com/app/?username=vtrans_row&lat='
+    var mapillary2nd = '&lng='
+    var mapillary3rd = '&z='
+    var mapillarySigns = '&signs=true'
 
-	var mapillaryURL = mapillarypre + lat + mapillary2nd + lon + mapillary3rd + '17' + mapillarySigns
-	window.open(mapillaryURL, "_blank")
+    var mapillaryURL = mapillarypre + lat + mapillary2nd + lon + mapillary3rd + '17' + mapillarySigns
+    window.open(mapillaryURL, "_blank")
 
 }
 
@@ -27,31 +27,33 @@ require([
     "esri/Map",
     "esri/views/MapView",
     "esri/layers/VectorTileLayer",
-		"esri/layers/FeatureLayer",
+    "esri/layers/FeatureLayer",
     "esri/geometry",
     "esri/core/urlUtils",
     "esri/geometry/support/webMercatorUtils",
-		"esri/geometry/Extent",
-		"esri/tasks/support/Query",
-		"esri/WebMap",
+    "esri/geometry/Extent",
+    "esri/tasks/support/Query",
+    "esri/WebMap",
     "esri/Graphic",
+    "esri/widgets/BasemapToggle"]
     "dojo/domReady!"
-  ], function(
-	  Map,
-	  MapView,
-	  VectorTileLayer,
-	  FeatureLayer,
-	  Geometry,
-	  urlUtils,
-	  webMercatorUtils,
-	  Extent,
-	  Query,
-	  WebMap,
-    Graphic
-  ) {
-	  window['Geometry'] = Geometry;
-	  window['Extent'] = Extent;
-	  window['Query'] = Query;
+], function(
+        Map,
+        MapView,
+        VectorTileLayer,
+        FeatureLayer,
+        Geometry,
+        urlUtils,
+        webMercatorUtils,
+        Extent,
+        Query,
+        WebMap,
+        Graphic,
+        BasemapToggle
+) {
+      window['Geometry'] = Geometry;
+      window['Extent'] = Extent;
+      window['Query'] = Query;
 
     var lon = -72.6
     var lat = 44
@@ -59,7 +61,7 @@ require([
     //if there are url params zoom to location
          var lon, lat, zoom;
          var urlObject = urlUtils.urlToObject(document.location.href);
-				 // for dev ?lon=-72.683117&lat=44.296882&zoomLevel=18
+                 // for dev ?lon=-72.683117&lat=44.296882&zoomLevel=18
         if(urlObject.query && urlObject.query.lon && urlObject.query.lat && urlObject.query.zoomLevel){
           lon = parseFloat(urlObject.query.lon);
           lat = parseFloat(urlObject.query.lat);
@@ -101,107 +103,107 @@ require([
     //       });
     // map.add(mapillary);
 
-	signLayer = new FeatureLayer("https://maps.vtrans.vermont.gov/arcgis/rest/services/AMP/Sign_Symbols/FeatureServer/0", {
-		mode: FeatureLayer.MODE_ONDEMAND,
-		outFields: ["*"],
-		id: 'signs'
-	});
-	map.add(signLayer);
+    signLayer = new FeatureLayer("https://maps.vtrans.vermont.gov/arcgis/rest/services/AMP/Sign_Symbols/FeatureServer/0", {
+        mode: FeatureLayer.MODE_ONDEMAND,
+        outFields: ["*"],
+        id: 'signs'
+    });
+    map.add(signLayer);
 
-	view.on('click', function(evt) {
-		handlePopup(evt);
-		return false;
-	});
+    view.on('click', function(evt) {
+        handlePopup(evt);
+        return false;
+    });
 });
 
 function handlePopup(evt) {
-	var latitude = evt.mapPoint.latitude,
-		longitude = evt.mapPoint.longitude;
+    var latitude = evt.mapPoint.latitude,
+        longitude = evt.mapPoint.longitude;
 
-	var extent = getExtent(evt.mapPoint, 20);
+    var extent = getExtent(evt.mapPoint, 20);
 
-	var query = new Query();
-	query.geometry = extent;
-	query.spatialRelationship = "intersects";
+    var query = new Query();
+    query.geometry = extent;
+    query.spatialRelationship = "intersects";
 
-	view.whenLayerView(signLayer).then(function (signs) {
-		signs.queryFeatures(query).then(function(results) {
-			buildPopup(latitude, longitude, results);
-		});
-	});
+    view.whenLayerView(signLayer).then(function (signs) {
+        signs.queryFeatures(query).then(function(results) {
+            buildPopup(latitude, longitude, results);
+        });
+    });
 }
 
 function getExtent(point, tol) {
-	var pixelWidth = view.extent.width / view.width;
-	var toleranceInMapCoords = tol * pixelWidth;
-	return new Geometry.Extent( point.x - toleranceInMapCoords,
-		   point.y - toleranceInMapCoords,
-		   point.x + toleranceInMapCoords,
-		   point.y + toleranceInMapCoords,
-		   point.spatialReference );
+    var pixelWidth = view.extent.width / view.width;
+    var toleranceInMapCoords = tol * pixelWidth;
+    return new Geometry.Extent( point.x - toleranceInMapCoords,
+           point.y - toleranceInMapCoords,
+           point.x + toleranceInMapCoords,
+           point.y + toleranceInMapCoords,
+           point.spatialReference );
 }
 
 function buildPopup(lat, lon, signs) {
-	var coordinates = {"latitude": lat, "longitude":lon};
+    var coordinates = {"latitude": lat, "longitude":lon};
 
-	document.getElementsByClassName('mapillary')[0].data = coordinates;
-	document.getElementsByClassName('coordinateCopier')[0].data = coordinates;
+    document.getElementsByClassName('mapillary')[0].data = coordinates;
+    document.getElementsByClassName('coordinateCopier')[0].data = coordinates;
 
-	document.getElementsByClassName('coordinates')[0].innerHTML = Math.round(lat*100000)/100000 + ', ' + Math.round(lon*100000)/100000;
+    document.getElementsByClassName('coordinates')[0].innerHTML = Math.round(lat*100000)/100000 + ', ' + Math.round(lon*100000)/100000;
 
-	//clean up old .signInfos
-	var signInfos = document.getElementsByClassName('signInfo');
+    //clean up old .signInfos
+    var signInfos = document.getElementsByClassName('signInfo');
 
-	while(signInfos[0]) {
-		signInfos[0].parentNode.removeChild(signInfos[0]);
-	}
-	//build new signinfos
+    while(signInfos[0]) {
+        signInfos[0].parentNode.removeChild(signInfos[0]);
+    }
+    //build new signinfos
 
-	for (var i = 0; i < signs.length; i++) {
-		var geo = signs[i].geometry,
-			attr = signs[i].attributes,
-			data = attr,
-			name = '';
-			signInfo = document.createElement("DIV");
+    for (var i = 0; i < signs.length; i++) {
+        var geo = signs[i].geometry,
+            attr = signs[i].attributes,
+            data = attr,
+            name = '';
+            signInfo = document.createElement("DIV");
 
-		signInfo.className = "signInfo";
-		signInfo.id = "sign" + attr.ID;
+        signInfo.className = "signInfo";
+        signInfo.id = "sign" + attr.ID;
 
-		data.latitude = geo.latitude;
-		data.longitude = geo.longitude;
+        data.latitude = geo.latitude;
+        data.longitude = geo.longitude;
 
-		name += data.MUTCDCode;
-		name += ' at ' + data.Marker;
-		name += ' on ' + data.STREETNAME.substring(data.STREETNAME.indexOf(',') + 1);
-		name += ' ' + data.LaneDirection + ' ';
+        name += data.MUTCDCode;
+        name += ' at ' + data.Marker;
+        name += ' on ' + data.STREETNAME.substring(data.STREETNAME.indexOf(',') + 1);
+        name += ' ' + data.LaneDirection + ' ';
 
 
 
-		signName = document.createElement("P");
-		signName.innerHTML = name;
+        signName = document.createElement("P");
+        signName.innerHTML = name;
 
-		copyButton = document.createElement("BUTTON");
-		copyButton.innerHTML = "Copy";
-		copyButton.data = JSON.stringify(data);
-		copyButton.onclick = function() {
-			copy(this.data);
-		};
+        copyButton = document.createElement("BUTTON");
+        copyButton.innerHTML = "Copy";
+        copyButton.data = JSON.stringify(data);
+        copyButton.onclick = function() {
+            copy(this.data);
+        };
 
-		signInfo.append(signName);
-		signName.append(copyButton);
+        signInfo.append(signName);
+        signName.append(copyButton);
 
-		document.getElementById('info').append(signInfo);
-	}
+        document.getElementById('info').append(signInfo);
+    }
 }
 
 function copyCoordinates() {
-	var data = document.getElementsByClassName('coordinateCopier')[0].data || view.center,
-		lat = data.latitude,
-		lon = data.longitude;
+    var data = document.getElementsByClassName('coordinateCopier')[0].data || view.center,
+        lat = data.latitude,
+        lon = data.longitude;
 
-		coordinates = {"latitude": lat, "longitude":lon};
+        coordinates = {"latitude": lat, "longitude":lon};
 
-	copy(JSON.stringify(coordinates));
+    copy(JSON.stringify(coordinates));
 }
 
 
